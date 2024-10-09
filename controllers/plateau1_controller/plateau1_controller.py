c9b01fd41d4cc7a478ef8a6317f71329b39a2aef
Lutecea <oceane.piatpro@gmail.com>
2024-10-09 16:29:45 +0200

    Rajout des commandes

--- controllers/plateau1_controller/plateau1_controller.py
+++ controllers/plateau1_controller/plateau1_controller.py
@@ -2,17 +2,31 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+from controller import Robot, Motor
+
+
+class BobRobot: 
+    right_wheel = None
+    left_wheel = None
 
 # create the Robot instance.
-robot = Robot()
+robot = BobRobot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
+motor1 = robot.getDevice('left wheel')
+motor2 = robot.getDevice('right wheel')
+
+motor1.setPosition(float('inf'))
+motor1.setVelocity(0.0)
+
+motor2.setPosition(float('inf'))
+motor2.setVelocity(0.0)
+
+
 #  ds = robot.getDevice('dsname')
 #  ds.enable(timestep)
 
@@ -22,9 +36,9 @@ while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
-
+    motor1.setVelocity(10.0)
     # Process sensor data here.
-
+    motor2.setVelocity(-10.0)
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
     pass
